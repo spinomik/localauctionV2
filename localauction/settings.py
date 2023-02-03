56f573d395b4bb103c6c306d49bc551f9e87313a
Spinomik <spinomik@onet.pl>
2023-02-03 15:05:30 +0100

    addDBconnection

--- localauction/settings.py
+++ localauction/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'localauction.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'localauction',
+    'USER': 'localauction',
+    'PASSWORD': 'VtiMpskO4v8y',
+    'HOST': 'ep-mute-dew-336962.eu-central-1.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
 
 
